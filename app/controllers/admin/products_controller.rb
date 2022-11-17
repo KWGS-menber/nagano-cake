ab958f0aaf60c665c522a851099f7ce85a9c94db
Yusuke Kashiwagi <114566645+Waggy-Music@users.noreply.github.com>
2022-11-17 16:29:08 +0000

    productNewView

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,21 @@
 class Admin::ProductsController < ApplicationController
+  def new
+    @product=Product.new
+  end
+  def show
+    
+  end
+  
+  def create
+  product=Product.new
+  product.save
+  end
+  
+  
+  
+  private
+  
+  def product_params
+    params.require(:product).permit(:genre_id,:name,:price,:explanation,:is_active,)
+  end
 end
