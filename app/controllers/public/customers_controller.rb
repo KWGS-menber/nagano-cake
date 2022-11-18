6221f68004da2b1d97e870a94d961670bb96337a
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-11-18 09:32:16 +0000

    [Add]会員登録

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,38 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
+  #マイページへのアクション
+  def show
+   @customer = current_customer
+  end
+
+  #登録情報編集へのアクション
+  def edit
+   @customer = current_customer
+  end
+
+  #登録情報の編集を保存するアクション(フラッシュメッセージ入れる？)
+  def update
+   @customer = current_customer
+   if @customer.update(customer_params)
+    redirect_to customer_path(@customer)
+   else
+    render 'edit'
+  end
+
+ #登録情報編集画面から退会ページを表示するアクション
+  def unsubscribe
+  end
+
+  #退会アクション
+  def is_deleted
+  end
+
+
+
+  private
+
+  def costomer_params
+   params.require(:costomer).permit(:first_name, :last_name, :first_kana, :last_kana, :zip, :address, :phone_number, :email)
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,22 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #アソシエーション
+  has_many :orders, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :deliveries, dependent: :destroy
+
+  #バリテーション
+  validates :first_name, presence:true, length: { minimum: 1, maximum: 20 }
+  validates :last_name, presence:true, length: { minimum: 1, maximum: 20 }
+  validates :first_kana, presence:true, length: { minimum: 1, maximum: 20 }
+  validates :last_kana, presence:true, length: { minimum: 1, maximum: 20 }
+  validates :zip, presence:true
+  validates :address, presence:true
+  validates :phone_number, presence:true
+  validates :email, presence:true
+
+  #def active_for_authentication?
+
 end
