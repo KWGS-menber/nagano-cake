5761e88569a4dc2b554064b6d8041c54a86cff7b
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-11-19 07:37:50 +0000

    [Add]admin => admin/sessions/new,admin/sessions_controller

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+  protected
+  def after_sign_in_path_for(resource)
+    admin_prod_path
+  end
 
   def after_sign_out_path_for(resource)
     new_admin_session_path
