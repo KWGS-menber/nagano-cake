72e614398d2a7f0797d8279202de9b962923056f
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-11-20 13:02:20 +0000

    fix

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,10 +1,6 @@
 class Genre < ApplicationRecord
-  # devise :detabase_authenticatable, : authentication_keys => [:name], :database_authenticatable, :registerable,
-  #       :recoverable, :rememberable, :validatable
-
   has_many :products
   validates :name, presence: true, uniqueness: true
   validates :email, presence: false
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_18_041649) do
+ActiveRecord::Schema.define(version: 2022_11_20_124659) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,18 +53,11 @@ ActiveRecord::Schema.define(version: 2022_11_18_041649) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.integer "customer_id", null: false
     t.integer "product_id", null: false
     t.string "product_count", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_cart_items_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_cart_items_on_reset_password_token", unique: true
   end
 
   create_table "customers", force: :cascade do |t|
@@ -88,56 +81,21 @@ ActiveRecord::Schema.define(version: 2022_11_18_041649) do
   end
 
   create_table "deliveries", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.integer "customer_id", null: false
     t.string "name", null: false
     t.string "zip", null: false
     t.string "address", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_deliveries_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_deliveries_on_reset_password_token", unique: true
-  end
-
-  create_table "dliveries", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "customer_id", null: false
-    t.string "name", null: false
-    t.string "zip", null: false
-    t.string "address", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_dliveries_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_dliveries_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_genres_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_genres_on_reset_password_token", unique: true
   end
 
   create_table "order_items", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.integer "product_id", null: false
     t.integer "order_id", null: false
     t.integer "product_count", null: false
@@ -145,16 +103,9 @@ ActiveRecord::Schema.define(version: 2022_11_18_041649) do
     t.integer "produntion_status", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_order_items_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_order_items_on_reset_password_token", unique: true
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.integer "customer_id", null: false
     t.string "name", null: false
     t.string "zip", null: false
@@ -165,16 +116,9 @@ ActiveRecord::Schema.define(version: 2022_11_18_041649) do
     t.integer "status", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_orders_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_orders_on_reset_password_token", unique: true
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.integer "genre_id", null: false
     t.string "name", null: false
     t.integer "price", null: false
@@ -182,20 +126,6 @@ ActiveRecord::Schema.define(version: 2022_11_18_041649) do
     t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_products_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_products_on_reset_password_token", unique: true
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
