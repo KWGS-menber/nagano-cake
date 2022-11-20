b297dfc8d09a3c3ebece73d1a66a9b8b20946f02
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-11-20 08:49:11 +0000

    [Update]adminログイン機能

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-  protected
-  def after_sign_in_path_for(resource)
-    admin_products_path
-  end
+  #protected
+  #def after_sign_in_path_for(resource)
+    #admin_products_path
+  #end
 
-  def after_sign_out_path_for(resource)
-    new_admin_session_path
-  end
+  #def after_sign_out_path_for(resource)
+    #new_admin_session_path
+  #end
 
   # before_action :configure_sign_in_params, only: [:create]
 
