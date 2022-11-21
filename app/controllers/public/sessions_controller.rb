ffbc9e3ee8f90c9582aafcba7af741925f0cc7a3
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-11-21 07:29:03 +0000

    [Add]admin/cutomers/index

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -35,7 +35,7 @@ class Public::SessionsController < Devise::SessionsController
     ##取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
        if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
           flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
-           redirect_to new_user_registration
+           redirect_to new_customer_registration_path
        end
     end
 end
