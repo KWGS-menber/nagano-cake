d31ef7f1536663f875548634fcbf71dcef8d3ad8
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-11-22 06:04:33 +0000

    [add] enum

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,24 @@
 class Public::OrdersController < ApplicationController
+  def new
+    @customer = current_customer
+  end
+
+  def confirm
+  end
+
+  def thanks
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+
+
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { wait: 0, confirmation: 1, make: 2, ready: 3, shipping: 4 }
 end
