95addba6e670fb78c40c4f419e4bc084369011e9
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-11-23 07:07:16 +0000

    [add,fix] orders_new, cart_items

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,17 +2,34 @@ class Public::OrdersController < ApplicationController
 
   def new
     @address = current_customer.address
-    @order =Order.new
+    @order = Order.new
   end
 
 
   def confirm
     @order = Order.new(order_params)
-    @address = Address.find(params[:order][:address_id])
-    @order.zip = @address.zip
-    @order.address = @address.address
-    @order.name = @address.name
     @cart_items = current_customer.cart_items.all
+    
+    if params[:order][:select_address] == "1" #自分の住所が選択された場合
+      @order.zip = current_customer.zip
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.farst_name
+    elsif params[:order][:select_address] == "2" #配送先の住所が選択された場合
+      delivery = Delivery.find(params[:order][:delivery_id])
+      @order.zip = delivery.zip
+      @order.address = delivery.address
+      @order.name = delivery.name
+    elsif params[:order][:select_address] == "3" #新規住所作成が選択された場合
+      @order.zip = params[:order][:delivery_zip]
+      @order.address = params[:order][:delivery_address]
+      @order.name = params[:order][:delivery_name]
+    else
+      render 'new'
+    end
+    # @address = Address.find(params[:order][:address_id])
+    # @order.zip = @address.zip
+    # @order.address = @address.address
+    # @order.name = @address.name
 
   end
 
@@ -20,6 +37,13 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    @order = current_customer.order.new(order_params)
+    @order.save
+    flash[:notice] = ""
+    redirect_to orders_thanks_path
+    if params[:order][:select_address] == "3"
+      current_customer.delivery_address.create(address_params)
+    end
   end
 
   def index
@@ -30,11 +54,15 @@ class Public::OrdersController < ApplicationController
    @order = Order.find(params[:id])
    @order_items = @order.order_items
   end
-  
+
   private
-  
-  
+
+
   def order_params
     params.require(:order).permit( :name, :zip, :address,:total_price,:postage,:payment_method,:status, :select_address)
+  end
+
+  def address_params
+    params.require(:order).permit(:zip, :adderss, :name)
   end
 end
