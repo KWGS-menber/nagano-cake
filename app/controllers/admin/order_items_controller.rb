990410e4067c76898043e4bc58db77343e300de1
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-11-26 05:04:34 +0000

    [fix] admin/order.show

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -3,15 +3,11 @@ class Admin::OrderItemsController < ApplicationController
 
   def update
     @order_item=OrderItem.find(params[:id])
+    @order = @order_item.order
     @order_item.update(order_item_params)
     flash[:notice]="変更を修正しました"
-    redirect_to admin_order_path
+    redirect_to admin_order_path(@order)
   end
-
-
-
-
-
 
   private
 
