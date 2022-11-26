9ef56fe7f41beb0097dc3a5d6f862ec20666c02a
EC2 Default User <ec2-user@ip-172-31-16-16.ap-northeast-1.compute.internal>
2022-11-26 06:37:56 +0000

    [fix] admin/order/show

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -5,6 +5,13 @@ class Admin::OrderItemsController < ApplicationController
     @order_item=OrderItem.find(params[:id])
     @order = @order_item.order
     @order_item.update(order_item_params)
+    if @order_item.production_status == "in_production"
+      @order.update(status: "make")
+    elsif @order.order_items.count == @order.order_items.where(production_status: "completion_of_production").count
+      @order.update(status: "ready")
+      # まずは.countを用いて、数を数えてあげる
+      # その後、whereを使って条件指定をしてあげる
+    end
     flash[:notice]="変更を修正しました"
     redirect_to admin_order_path(@order)
   end
